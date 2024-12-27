7be9e2fe45a30602305519c3ff56120beaf6b21e
cs99x <schmidcs@mailbox.org>
2024-12-27 20:59:41 +0100

    Flask backend

--- backend/app/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    DATABASE_URL: str = "sqlite:///./sensor_data.db"
-
-    class Config:
-        env_file = ".env"
-
-settings = Settings()
---
+++ backend/my-flask-app/config.py
@@ -0,0 +1,5 @@
+class Config:
+    DEBUG = True
+    SECRET_KEY = 'your_secret_key_here'
+    JSON_SORT_KEYS = False
+    # Add other configuration settings as needed
