022c0a8f5dd4eeb17a896c25f842a00237949405
cs99x <schmidcs@mailbox.org>
2024-12-27 16:03:02 +0100

    Fix repo

--- mobile/Gasmeter/Gasmeter/DeviceData.swift
+++ mobile/Gasmeter/Gasmeter/DeviceData.swift
@@ -1,7 +1,31 @@
-//
-//  DeviceData.swift
-//  Gasmeter
-//
-//  Created by Christoph Schmid on 26.12.24.
-//
+struct DeviceData: Codable {
+    let id: String
+    let ts: String
+    let batt: BatteryStatus
+    let sensors: [SensorReading]
+    let temp: Temperature
+    let stat: Status
 
+    struct BatteryStatus: Codable {
+        let pct: Int
+        let chg: Bool
+    }
+
+    struct SensorReading: Codable {
+        let type: String
+        let name: String
+        let unit: String
+        let val: Double
+    }
+
+    struct Temperature: Codable {
+        let type: String
+        let unit: String
+        let val: Double
+    }
+
+    struct Status: Codable {
+        let alarm: Bool
+        let fault: Bool
+    }
+}
